abba1f3886da270f95fec7526b67527aa81e7002
Karlo Cehulic <karlocehlic@gmail.com>
2025-11-13 21:08:41 +0100

    feat: link empty demo page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="grid grid-rows-2 pt-[35vh] justify-center gap-5">
       <h1 className="text-5xl font-bold">AlgoFER</h1>
-      <button className="text-2xl bg-blue-100 rounded-sm justify-self-center p-1.5 text-indigo-950">
+      <Link
+        href={"/demo"}
+        className="text-2xl bg-blue-100 rounded-sm justify-self-center p-1.5 text-indigo-950"
+      >
         Demo
-      </button>
+      </Link>
     </div>
   );
 }
