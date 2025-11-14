5f678819fca2d76ed28fa6fc6692f4a9ab46d363
Karlo Cehulic <karlocehlic@gmail.com>
2025-11-14 01:14:15 +0100

    feat(server): initialize SpringBoot server

---
+++ server/src/main/java/com/hr/fer/algofer/AlgoferApplication.java
@@ -0,0 +1,13 @@
+package com.hr.fer.algofer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoferApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AlgoferApplication.class, args);
+  }
+
+}
