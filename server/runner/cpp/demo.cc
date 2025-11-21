ff8a44a957c6cb802810c7acd2e25be8fc7cbf52
Karlo Cehulic <karlocehlic@gmail.com>
2025-11-22 00:50:28 +0100

    refactor(runner): extract mylist class
    This is also where users code will be copied onto

--- server/runner/cpp/demo.cc
+++ server/runner/cpp/demo.cc
@@ -1,4 +1,5 @@
 #include "list_element.h"
+#include "mylist.h"
 #include "split_array.h"
 #include <iostream>
 #include <stdexcept>
@@ -64,69 +65,6 @@ public:
 
     head = dummy->next; // in case head is delted
     delete dummy;
-  }
-
-  bool Contains(T val) {
-    ListElement<T> *curr = head;
-
-    while (curr != nullptr) {
-      if (curr->i == val) {
-        return true;
-      }
-      curr = curr->next;
-    }
-
-    return false;
-  }
-};
-
-// INSERT HERE
-template <typename T> class MyList {
-private:
-  ListElement<T> *head;
-
-public:
-  MyList() { this->head = nullptr; };
-
-  void AddUnique(T val) {
-    // if (this->Contains(val)) {
-    //   return;
-    // }
-
-    ListElement<T> *dummy = new ListElement<T>();
-    dummy->next = head;
-
-    ListElement<T> *curr = dummy;
-
-    while (curr->next != nullptr) {
-      curr = curr->next;
-    }
-
-    ListElement<T> *newUnique = new ListElement<T>;
-    newUnique->i = val;
-    curr->next = newUnique;
-
-    head = dummy->next;
-  }
-
-  void Remove(T val) {
-    ListElement<T> *dummy = new ListElement<T>();
-    dummy->next = head;
-
-    ListElement<T> *curr = dummy;
-
-    while (curr->next != nullptr) {
-      ListElement<T> *nextPtr = curr->next;
-      if (nextPtr->i == val) {
-        curr->next = nextPtr->next;
-        delete nextPtr;
-        break;
-      }
-
-      curr = nextPtr;
-    }
-
-    head = dummy->next; // in case head is delted
   }
 
   bool Contains(T val) {
