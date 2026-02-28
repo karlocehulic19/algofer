599d6a843f3e2a78afba03830db1e95316c308c0
Karlo Cehulic <karlocehlic@gmail.com>
2026-02-28 01:05:05 +0100

    feat(client): separate button for OOP demo

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,14 +2,22 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-2 pt-[35vh] justify-center gap-5">
-      <h1 className="text-5xl font-bold">AlgoFER</h1>
-      <Link
-        href={"/demo"}
-        className="text-2xl bg-blue-100 rounded-sm justify-self-center p-1.5 text-indigo-950"
-      >
-        Demo
-      </Link>
+    <div className="grid pt-[35vh] justify-center gap-5">
+      <h1 className="text-5xl font-bold pb-10 text-center">AlgoFER</h1>
+      <div className="flex gap-10 justify-center flex-col">
+        <Link
+          href={"/oop/demo"}
+          className="text-2xl bg-blue-100 rounded-sm justify-self-center p-1.5 text-indigo-950 text-center w-[20vw]"
+        >
+          OOP
+        </Link>
+        <Link
+          href={"/asp/demo"}
+          className="text-2xl bg-blue-100 rounded-sm justify-self-center p-1.5 text-indigo-950 text-center w-[20vw]"
+        >
+          ASP
+        </Link>
+      </div>
     </div>
   );
 }
