9e37099e2e115bc1284407a2322a591b4b3f9003
Karlo Cehulic <karlocehlic@gmail.com>
2026-02-28 01:05:12 +0100

    feat(client): real OOP zad 1 vj-1-2 text

--- client/src/components/demo/Content.tsx
+++ client/src/components/demo/Content.tsx
@@ -3,37 +3,18 @@ import { Category } from "../Category";
 export function Content() {
   return (
     <div className="flex flex-3 bg-blue-100 text-black p-7 gap-2 flex-col">
-      <h1 className="text-3xl underline">Implementiraj Linearnu Listu</h1>
+      <h1 className="text-3xl underline">Zaokruzi PI</h1>
       <div className="flex gap-2.5">
-        <Category>MI-24/25</Category>
-        <Category>Zad 2</Category>
-        <Category>Linearna Lista</Category>
+        <Category>VJ-1-2</Category>
+        <Category>Zad 1</Category>
+        <Category>System Out</Category>
       </div>
       <pre className="whitespace-break-spaces">
         {`
-Potrebno je implementirati metodu AddUnique, Contains i Remove jednostruko povezane linearne liste implementirane pokazivačima.
-
-Lista treba biti ostvarena korištenjem predložaka (eng. templates). Metoda AddUnique dodaje novi element na kraj liste samo ako element već ne postoji, metoda Remove uklanja prvi element liste sa zadanom vrijednosti, a metoda Contains provjerava nalazi li se određeni element u listi.
-
-Atom liste ListElement i prototipovi funkcija zadani
-su odsječcima koda u nastavku.
-
+Broj π može se izračunati preko sljedeće formule: 𝜋 4 = ∑ (−1) 𝑖 2𝑖+1 ∞ 𝑖=0
+Ispisati izračunatu vrijednost zaokruženo na 10 decimala nakon 10, 100, 1000, … i 1 000 000 sumanada.
 `}
       </pre>
-
-      <div className="flex gap-3 flex-col">
-        <h2 className="text-xl">Prototipovi:</h2>
-        <div className="flex">
-          <pre className="p-1 bg-blue-50">
-            {`template<typename T>
-class ListElement{
-  public:
-    T i;
-    ListElement<T>* next;
-};`}
-          </pre>
-        </div>
-      </div>
     </div>
   );
 }
