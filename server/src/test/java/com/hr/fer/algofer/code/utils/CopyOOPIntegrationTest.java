da3919730e84f1e564bb9e90504ecceb235b0d81
Karlo Cehulic <karlocehlic@gmail.com>
2026-03-09 22:35:49 +0100

    test(server): copy code copies text

---
+++ server/src/test/java/com/hr/fer/algofer/code/utils/CopyOOPIntegrationTest.java
@@ -0,0 +1,41 @@
+package com.hr.fer.algofer.code.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource("classpath:code/utils/copy.test.properties")
+class AlgoferApplicationTests {
+  @Value("${COMPARE_COPIED_FILENAME}")
+  String compareFilename;
+  @Value("${COPIED_OOP_DIR}")
+  String copiedDir;
+  @Value("${COPIED_OOP_FILENAME}")
+  String copiedFilename;
+
+  @Autowired
+  CopyOOPCode copyOOPCode;
+
+  @Test
+  void testCopying() throws IOException {
+    File copied = new ClassPathResource(copiedDir + "/" + copiedFilename).getFile();
+
+    copyOOPCode.copy("Hello World!", copied.getAbsolutePath());
+
+    File expected = new ClassPathResource(copiedDir + "/" + compareFilename).getFile();
+    assertEquals(FileUtils.readLines(copied, Charset.defaultCharset()),
+        FileUtils.readLines(expected, Charset.defaultCharset()));
+  }
+
+}
