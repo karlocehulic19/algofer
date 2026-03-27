0aca04aae22c776192b43ee2caca6ffa7ec8dde5
Karlo Cehulic <karlocehlic@gmail.com>
2026-03-27 23:30:01 +0100

    feat(server-go): POST endpoint for code

--- server-go/main.go
+++ server-go/main.go
@@ -8,9 +8,28 @@ func getHello(c *gin.Context) {
 	c.String(200, "Hello, AlgoFER user!")
 }
 
+type CodeSubmit struct {
+	Code string `json:"code" binding:"required"`
+}
+
+func postDemoSolution(c *gin.Context) {
+	var codeSubmit CodeSubmit
+
+	if err := c.ShouldBindJSON(&codeSubmit); err != nil {
+		c.String(400, "Invalid JSON")
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"status":     "PASS",
+		"codeSubmit": codeSubmit,
+	})
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/hello", getHello)
+	router.POST("/api/v1/oop/demo/submit", postDemoSolution)
 
 	router.Run("localhost:8080")
 }
