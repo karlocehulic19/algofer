9c99eac7993d5aae3c6b4196635b090609affa02
Karlo Cehulic <karlocehlic@gmail.com>
2026-04-01 23:56:54 +0200

    feat(server-go): compile and run Java

---
+++ server-go/compiler/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+  public static void main(String[] args) {
+    Solution.zaokruziPi();
+  }
+
+  public static void zaokruziPi() {
+    System.out.println("Hello World!");
+    // add your solution here
+  }
+}
